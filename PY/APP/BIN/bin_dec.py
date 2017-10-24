837932ec18ff1d7711f2f5c76e6f2036a240ce71
ttltrk <ttltrk.acc@gmail.com>
2017-10-24 08:11:25 +0200

    Update bin_dec.py

--- PY/APP/BIN/bin_dec.py
+++ PY/APP/BIN/bin_dec.py
@@ -1,19 +1,7 @@
 
-"""
-1024
-512
-256
-128
-64
-32
-16
-8
-4
-2
-1
-in progres....
-"""
-
 a = int(input("number:"))
 
-print(len(str(abs(a))))
+def bin():
+  return(len(str(abs(a))))
+  
+print(bin())
