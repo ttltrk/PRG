d39eff63284a7986e5c634a047a1b56513e037f5
ttltrk <ttltrk.acc@gmail.com>
2017-10-24 08:15:04 +0200

    Update bin_dec.py

--- PY/APP/BIN/bin_dec.py
+++ PY/APP/BIN/bin_dec.py
@@ -1,7 +1,31 @@
 
+"""
+1024
+512
+256
+128
+64
+32
+16
+8
+4
+2
+1
+"""
+
 a = int(input("number:"))
 
-def bin():
+def check():
   return(len(str(abs(a))))
+
+b = check()
   
-print(bin())
+def check2():
+  if b == 1:
+    print("1")
+  elif b == 2:
+    print("2")
+  else:
+    print("3")
+
+check2()
