446d13ad8b09a29edfc70a2f443ef53316be16cd
ttltrk <ttltrk.acc@gmail.com>
2017-10-24 09:51:28 +0200

    Update bin_dec.py

--- PY/APP/BIN/bin_dec.py
+++ PY/APP/BIN/bin_dec.py
@@ -17,11 +17,13 @@ lst1 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
 num = int(input("number?"))
 lst2 = [int(i) for i in str(num)]
-
+  
 def check():
   if lst2 == 1:
-    print (lst1[0]*num)
+    print(lst1[0]*num)
   elif lst2 == 2:
-    print ((lst1[0]*lst2[0])+(lst1[1]*lst2[1]))
+    print((lst1[0]*lst2[0])+(lst1[1]*lst2[1]))
+  elif lst2 == 3:
+    print((lst1[2]*lst2[0])+(lst1[1]*lst2[1])+(lst1[0]*lst2[2]))
   
 check()
