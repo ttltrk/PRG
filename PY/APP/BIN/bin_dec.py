1323707aa4909fdcd4db763e994491d1415ba3f4
ttltrk <ttltrk.acc@gmail.com>
2017-10-24 09:22:49 +0200

    Update bin_dec.py

--- PY/APP/BIN/bin_dec.py
+++ PY/APP/BIN/bin_dec.py
@@ -13,9 +13,11 @@
 1
 """
 
-num = int(input("number?"))
-lst = [int(i) for i in str(num)]
+lst1 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
-print(len(lst))
-print(lst[1])
-print(lst[1]*2)
+num = int(input("number?"))
+lst2 = [int(i) for i in str(num)]
+
+print(len(lst2))
+print(lst2[1])
+print(lst2[1]*lst1[1])
