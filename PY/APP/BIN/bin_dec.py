90e06e2dea6d22e81d134c62ff13edc76de5a1f4
ttltrk <ttltrk.acc@gmail.com>
2017-10-24 10:01:50 +0200

    Update bin_dec.py

--- PY/APP/BIN/bin_dec.py
+++ PY/APP/BIN/bin_dec.py
@@ -32,7 +32,7 @@ def check():
   if l == 1:
     print(lst1[0]*num)
   elif l == 2:
-    print((lst1[0]*lst2[0])+(lst1[1]*lst2[1]))
+    print((lst1[1]*lst2[0])+(lst1[0]*lst2[1]))
   elif l == 3:
     print((lst1[2]*lst2[0])+(lst1[1]*lst2[1])+(lst1[0]*lst2[2]))
   
