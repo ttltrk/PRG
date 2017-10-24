e8903d72c810f73b98e03ff4f90f4ab66ee2f51a
ttltrk <ttltrk.acc@gmail.com>
2017-10-24 21:58:51 +0200

    Update bin_dec.py

--- PY/APP/BIN/bin_dec.py
+++ PY/APP/BIN/bin_dec.py
@@ -1,6 +1,5 @@
 
 lst1 = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-
 num = int(input("give me a number between 0 and 11111111, you can only use binary numbers: \n"))
 lst2 = [int(i) for i in str(num)]
 
@@ -21,6 +20,10 @@ def length():
     return 7
   elif len(lst2) == 8:
     return 8
+  elif len(lst2) == 9:
+    return 9
+  elif len(lst2) == 10:
+    return 10
   
 l = length()
 
@@ -41,5 +44,9 @@ def check():
     print((lst1[6]*lst2[0])+(lst1[5]*lst2[1])+(lst1[4]*lst2[2])+(lst1[3]*lst2[3])+(lst1[2]*lst2[4])+(lst1[1]*lst2[5])+(lst1[0]*lst2[6]))
   elif l == 8: 
     print((lst1[7]*lst2[0])+(lst1[6]*lst2[1])+(lst1[5]*lst2[2])+(lst1[4]*lst2[3])+(lst1[3]*lst2[4])+(lst1[2]*lst2[5])+(lst1[1]*lst2[6])+(lst1[0]*lst2[7]))
-  
+  elif l == 9: 
+    print((lst1[8]*lst2[0])+(lst1[7]*lst2[1])+(lst1[6]*lst2[2])+(lst1[5]*lst2[3])+(lst1[4]*lst2[4])+(lst1[3]*lst2[5])+(lst1[2]*lst2[6])+(lst1[1]*lst2[7])+(lst1[0]*lst2[8]))
+  elif l == 10:  
+    print((lst1[9]*lst2[0])+(lst1[8]*lst2[1])+(lst1[7]*lst2[2])+(lst1[6]*lst2[3])+(lst1[5]*lst2[4])+(lst1[4]*lst2[5])+(lst1[3]*lst2[6])+(lst1[2]*lst2[7])+(lst1[1]*lst2[8])+(lst1[0]*lst2[9]))
+
 check()
